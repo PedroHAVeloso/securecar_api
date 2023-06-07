dc80bb9207dd7f0deafde4120571895695a01af8
Pedro Henrique Alves Veloso <86307207+PedroHAVeloso@users.noreply.github.com>
2023-06-07 20:31:14 -0300

    Adição de alguns headers.

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ require_once 'vendor/autoload.php';
 
 use App\Routes\Routes;
 
+header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, PATCH, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
 Routes::enterApi();
 
