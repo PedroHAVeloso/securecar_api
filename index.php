d478425a37a337e4bb2eb53b0e1c5197cc6343f5
Pedro Henrique Alves Veloso <86307207+PedroHAVeloso@users.noreply.github.com>
2023-06-07 20:32:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,9 @@ use App\Routes\Routes;
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, PATCH, OPTIONS");
+header("Access-Control-Allow-Credentials: false");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
-Routes::enterApi();
+// Routes::enterApi();
 
 ?>
